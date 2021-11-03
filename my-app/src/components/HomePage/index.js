9fbbcab5be6eaa3c0d7102ead4522b00c54f5031
Catherine Cox <87046645+beachbrunet@users.noreply.github.com>
2021-11-03 15:48:00 -0600

    added git ignore and basic code for homepage

---
+++ my-app/src/components/HomePage/index.js
@@ -0,0 +1,21 @@
+// about me
+import React from "react";
+import "./home.css";
+import image from "../../assets/catherine.jpeg";
+
+const homePage = () => {
+  return (
+    <section className="intro">
+      <div className="col-md-5 mx-auto">
+        <h1 className=""> Catherine Cox </h1>
+      </div>
+      <p></p>
+    </section>
+  );
+};
+
+export default homePage;
+
+{
+  /* <img src={image} width="100" alt="profile pic" /> */
+}
